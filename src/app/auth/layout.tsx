e1f2f1157e9ebca46859a7575652666e52d49f6d
TIHeitorDS <heitorclaudinods@gmail.com>
2025-05-24 22:36:02 -0300

    fix: remove unnecessary width and padding from AuthLayout component

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w- h-svh px-4 pt-[147px] bg-linear-to-b from-secondary to-third ">
+    <div className="h-svh px-4 bg-linear-to-b from-secondary to-third ">
       {children}
     </div>
   );
